2fce60f175f609bb02bdc5cc6d0241b8a5524719
DivyanshiVashist <s4354069@uq.net.au>
2017-10-01 00:46:47 +1000

    - Different screen for teacher and student accounts. Teacher screen is only for viewing the class game. - Based on question's difficulty represented by the stars the student upon answering it correcty will be awarded that number of steps to move through the maze - Restricts the number of steps based on how many stars student has won.

--- ElectronicPrototype/maze_game/index.js
+++ ElectronicPrototype/maze_game/index.js
@@ -32,9 +32,11 @@ function canMove(x, y){
 
 var players = {};
 var names = {};
-var playerCount = 0;
+var playerCount = -1;
 var startX = [0, 0, 9, 9];
 var startY = [0, 9, 0, 9];
+
+var counters = {};
 
 function checkWin(x, y){
     return ((x == 4) && (y == 4));
@@ -44,38 +46,56 @@ function checkWin(x, y){
 // and write to console
 io.on('connection', function(socket){
     socket.on('new player', function() {
-        players[socket.id] = {
-            x: startX[playerCount],
-            y: startY[playerCount]
-        };
-        
-        names[socket.id] = playerCount;
-        
-        playerCount++;
-        io.sockets.emit('state', players);
-        io.sockets.emit('playerTitle', names[socket.id]);
+        if(playerCount == -1) {
+            io.sockets.emit('state', players);
+            io.sockets.emit('playerTitle', 'x');
+            playerCount++;
+        } else {
+            players[socket.id] = {
+                x: startX[playerCount],
+                y: startY[playerCount]
+            };
+            
+            names[socket.id] = playerCount;
+            counters[socket.id] = 0;
+            
+            playerCount++;
+            io.sockets.emit('state', players);
+            io.sockets.emit('playerTitle', names[socket.id]);
+            io.sockets.emit('counterUpdate', counters[socket.id]);
+        }
+
     });
     
+    socket.on("addCounter", function(amount) {
+        counters[socket.id] += amount;
+        io.sockets.emit('counterUpdate', counters[socket.id]);
+
+    });
     // On receiving message  
     socket.on('movement', function(msg){
         var player = players[socket.id] || {};
         
-        if ((msg == 1) && canMove(player.x, player.y-1))
-            player.y--;
-        else if ((msg == 2) && canMove(player.x, player.y+1))
-            player.y++;
-        else if ((msg == 3) && canMove(player.x-1, player.y))
-            player.x--;
-        else if ((msg == 4) && canMove(player.x+1, player.y))
-            player.x++;
+        if(counters[socket.id] > 0) {
+            if ((msg == 1) && canMove(player.x, player.y-1))
+                player.y--;
+            else if ((msg == 2) && canMove(player.x, player.y+1))
+                player.y++;
+            else if ((msg == 3) && canMove(player.x-1, player.y))
+                player.x--;
+            else if ((msg == 4) && canMove(player.x+1, player.y))
+                player.x++;
+                    
+            if(checkWin(player.x, player.y) == true) {
+                io.sockets.emit('win', names[socket.id]);
+            }
+            
+            counters[socket.id]--;
+        } 
         
         io.sockets.emit('state', players);
-        
-        if(checkWin(player.x, player.y) == true) {
-            io.sockets.emit('win', names[socket.id]);
+        io.sockets.emit('counterUpdate', counters[socket.id]);
 
-            //io.sockets.emit('state', players);
-        }
     });
     
     /*
--- ElectronicPrototype/maze_game/static/game.js
+++ ElectronicPrototype/maze_game/static/game.js
@@ -3,19 +3,28 @@ var socket = io();
 var movement = 0;
 
 
-$(document).keyup(function(e){
-    if((e.which == 38))//Up arrow
-        //player.y--;
-        movement = 1;
-    else if((e.which == 40)) // down arrow
-        //player.y++;
-        movement = 2;
-    else if((e.which == 37))
-        //splayer.x--;
-        movement = 3;
-    else if ((e.which == 39))
-        //player.x++;
-        movement = 4;
+$(document).on("click", "#up", function(){
+    movement = 1;
+    socket.emit('movement', movement);
+    movement = 0;
+});
+
+$(document).on("click", "#down", function(){
+    movement = 2;
+    socket.emit('movement', movement);
+    movement = 0;
+});
+
+
+$(document).on("click", "#left", function(){
+    movement = 3;
+    socket.emit('movement', movement);
+    movement = 0;
+});
+
+
+$(document).on("click", "#right", function(){
+    movement = 4;
     socket.emit('movement', movement);
     movement = 0;
 });
